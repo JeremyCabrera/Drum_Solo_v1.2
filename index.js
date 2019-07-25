6b115fe3681abeb7260cf117e05cbcfa19e24f94
Jeremy Cabrera <jeremy.cabrera@outlook.com>
2019-07-25 09:33:53 -0400

    Checking for image input

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-var numberOfDrumButtons = document.querySelector(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-for (var i = 0; i< numberOfDrumButtons; i++) {
-//Code below "finds" the first button in the document, then it adds an event, listens for the click then calls the method//
-document.querySelector("button").addEventListener("click", function(){
-  alert("I got clicked");
-});
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+      // location of mp3 file
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+  });
+
 }
